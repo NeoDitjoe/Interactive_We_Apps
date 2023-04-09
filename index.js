4dbb617adcfd05b1cc77c658036926acf01ee497
NeoDitjoe <ditjoeneo03@gmail.com>
2023-04-09 11:56:06 +0200

    modify

---
+++ index.js
@@ -0,0 +1,135 @@
+function color(value){
+    document.body.style.backgroundColor = value;
+    if (value === "rgba(128, 128, 128, 0.516)") {
+        document.getElementById("ditjoe").style.color = "white";
+        document.getElementById("education").style.color = "gray";
+        document.getElementById("education").style.backgroundColor = "white";
+        document.getElementById("kliptown").style.backgroundColor = "gray";
+        document.getElementById("simplilearn").style.backgroundColor = "gray";
+        document.getElementById("codespace").style.backgroundColor = "gray";
+        document.getElementById("experience").style.color = "gray";
+        document.getElementById("experience").style.backgroundColor = "white";
+        document.getElementById("jetline").style.backgroundColor = "gray";
+        document.getElementById("credico").style.backgroundColor = "gray";
+        document.getElementById("skills").style.color = "gray";
+        document.getElementById("skills").style.backgroundColor = "white";
+        document.getElementById("contact").style.color = "gray";
+    } 
+    else if (value === " rgba(255, 0, 0, 0.237)"){
+        document.getElementById("ditjoe").style.color = "red";
+        document.getElementById("education").style.color = "red";
+        document.getElementById("education").style.backgroundColor = "white";
+        document.getElementById("kliptown").style.backgroundColor = "rgba(255, 0, 0, 0.797)";
+        document.getElementById("simplilearn").style.backgroundColor = "rgba(255, 0, 0, 0.797)";
+        document.getElementById("codespace").style.backgroundColor = "rgba(255, 0, 0, 0.797)";
+        document.getElementById("experience").style.color = "red";
+        document.getElementById("experience").style.backgroundColor = "white";
+        document.getElementById("jetline").style.backgroundColor = "rgba(255, 0, 0, 0.797)";
+        document.getElementById("credico").style.backgroundColor = "rgba(255, 0, 0, 0.797)";
+        document.getElementById("skills").style.color = "red";
+        document.getElementById("skills").style.backgroundColor = "white";
+        document.getElementById("contact").style.color = "red";
+    }
+    else if (value === "rgba(0, 0, 255, 0.279)"){
+        document.getElementById("ditjoe").style.color = "blue";
+        document.getElementById("education").style.color = "blue";
+        document.getElementById("education").style.backgroundColor = "white";
+        document.getElementById("kliptown").style.backgroundColor = "rgba(0, 0, 255, 0.788)";
+        document.getElementById("simplilearn").style.backgroundColor = "rgba(0, 0, 255, 0.788)";
+        document.getElementById("codespace").style.backgroundColor = "rgba(0, 0, 255, 0.788)";
+        document.getElementById("experience").style.color = "blue";
+        document.getElementById("experience").style.backgroundColor = "white";
+        document.getElementById("jetline").style.backgroundColor = "rgba(0, 0, 255, 0.788)";
+        document.getElementById("credico").style.backgroundColor = "rgba(0, 0, 255, 0.788)";
+        document.getElementById("skills").style.color = "blue";
+        document.getElementById("skills").style.backgroundColor = "white";
+        document.getElementById("contact").style.color = "blue";
+    }
+    else if (value === "rgba(128, 0, 128, 0.461)"){
+        document.getElementById("ditjoe").style.color = "purple";
+        document.getElementById("education").style.color = "purple";
+        document.getElementById("education").style.backgroundColor = "white";
+        document.getElementById("kliptown").style.backgroundColor = "rgba(128, 0, 128, 0.795)";
+        document.getElementById("simplilearn").style.backgroundColor = "rgba(128, 0, 128, 0.795)";
+        document.getElementById("codespace").style.backgroundColor = "rgba(128, 0, 128, 0.795)";
+        document.getElementById("experience").style.color = "purple";
+        document.getElementById("experience").style.backgroundColor = "white";
+        document.getElementById("jetline").style.backgroundColor = "rgba(128, 0, 128, 0.795)";
+        document.getElementById("credico").style.backgroundColor = "rgba(128, 0, 128, 0.795)";
+        document.getElementById("skills").style.color = "purple";
+        document.getElementById("skills").style.backgroundColor = "white";
+        document.getElementById("contact").style.color = "purple";
+    }
+    else if (value === "rgba(0, 250, 0, 0.183)"){
+        document.getElementById("ditjoe").style.color = "green";
+        document.getElementById("education").style.color = "green";
+        document.getElementById("education").style.backgroundColor = "white";
+        document.getElementById("kliptown").style.backgroundColor = "rgba(0, 128, 0, 0.813)";
+        document.getElementById("simplilearn").style.backgroundColor = "rgba(0, 128, 0, 0.813)";
+        document.getElementById("codespace").style.backgroundColor = "rgba(0, 128, 0, 0.813)";
+        document.getElementById("experience").style.color = "green";
+        document.getElementById("experience").style.backgroundColor = "white";
+        document.getElementById("jetline").style.backgroundColor = "rgba(0, 128, 0, 0.813)";
+        document.getElementById("credico").style.backgroundColor = "rgba(0, 128, 0, 0.813)";
+        document.getElementById("skills").style.color = "green";
+        document.getElementById("skills").style.backgroundColor = "white";
+        document.getElementById("contact").style.color = "green";
+    }
+    else if (value === "rgba(255, 255, 0, 0.425)"){
+        document.getElementById("ditjoe").style.color = "yellow";
+        document.getElementById("education").style.color = "yellow";
+        document.getElementById("education").style.backgroundColor = "white";
+        document.getElementById("kliptown").style.backgroundColor = "rgba(255, 255, 0, 0.804)";
+        document.getElementById("simplilearn").style.backgroundColor = "rgba(255, 255, 0, 0.804)";
+        document.getElementById("codespace").style.backgroundColor = "rgba(255, 255, 0, 0.804)";
+        document.getElementById("experience").style.color = "yellow";
+        document.getElementById("experience").style.backgroundColor = "white";
+        document.getElementById("jetline").style.backgroundColor = "rgba(255, 255, 0, 0.804)";
+        document.getElementById("credico").style.backgroundColor = "rgba(255, 255, 0, 0.804)";
+        document.getElementById("skills").style.color = "yellow";
+        document.getElementById("contact").style.color = "yellow";
+    }
+    else if (value === "rgba(135, 207, 235, 0.338)"){
+        document.getElementById("ditjoe").style.color = "blue";
+        document.getElementById("education").style.color = "skyblue";
+        document.getElementById("education").style.backgroundColor = "white";
+        document.getElementById("kliptown").style.backgroundColor = "rgba(135, 207, 235, 0.808)";
+        document.getElementById("simplilearn").style.backgroundColor = "rgba(135, 207, 235, 0.808)";
+        document.getElementById("codespace").style.backgroundColor = "rgba(135, 207, 235, 0.808)";
+        document.getElementById("experience").style.color = "skyblue";
+        document.getElementById("experience").style.backgroundColor = "white";
+        document.getElementById("jetline").style.backgroundColor = "rgba(135, 207, 235, 0.808)";
+        document.getElementById("credico").style.backgroundColor = "rgba(135, 207, 235, 0.808)";
+        document.getElementById("skills").style.color = "skyblue";
+        document.getElementById("skills").style.backgroundColor = "white";
+        document.getElementById("contact").style.color = "skyblue";
+    }
+    else if (value === "white"){
+        document.getElementById("ditjoe").style.color = "blue";
+        document.getElementById("education").style.color = "white";
+        document.getElementById("kliptown").style.backgroundColor = "rgb(62, 62, 62)";
+        document.getElementById("simplilearn").style.backgroundColor = "rgb(62, 62, 62)";
+        document.getElementById("codespace").style.backgroundColor = "rgb(62, 62, 62)";
+        document.getElementById("experience").style.color = "white";
+        document.getElementById("jetline").style.backgroundColor = "rgb(62, 62, 62)";
+        document.getElementById("credico").style.backgroundColor = "rgb(62, 62, 62)";
+        document.getElementById("skills").style.color = "white";
+        document.getElementById("skills").style.backgroundColor = "rgb(62, 62, 62)";
+        document.getElementById("contact").style.color = "white";
+    }
+    else{
+        document.getElementById("ditjoe").style.color = "orange";
+        document.getElementById("education").style.color = "orange";
+        document.getElementById("education").style.backgroundColor = "white";
+        document.getElementById("kliptown").style.backgroundColor = " rgba(255, 166, 0, 0.824)";
+        document.getElementById("simplilearn").style.backgroundColor = " rgba(255, 166, 0, 0.824)";
+        document.getElementById("codespace").style.backgroundColor = " rgba(255, 166, 0, 0.824)";
+        document.getElementById("experience").style.color = "orange";
+        document.getElementById("experience").style.backgroundColor = "white";
+        document.getElementById("jetline").style.backgroundColor = " rgba(255, 166, 0, 0.824)";
+        document.getElementById("credico").style.backgroundColor = " rgba(255, 166, 0, 0.824)";
+        document.getElementById("skills").style.color = "orange";
+        document.getElementById("skills").style.backgroundColor = "white";
+        document.getElementById("contact").style.color = "orange";
+    }
+}
