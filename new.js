ac5eadd43872228522aecc7821f93db3ea5467f1
NeoDitjoe <ditjoeneo03@gmail.com>
2023-04-09 16:35:59 +0200

    click effect

---
+++ new.js
@@ -0,0 +1,15 @@
+
+function highlightFooter() {
+// Get the footer element
+const footer = document.querySelector('footer');
+
+// Add a class to change its background color
+footer.classList.add('highlight');
+
+
+setTimeout(function() {
+  footer.classList.remove('highlight');
+}, 500);
+}
+
+
